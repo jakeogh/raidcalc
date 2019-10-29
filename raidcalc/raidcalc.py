f3e4709906d8fd77486277806ec5ab4f332b7f6e
jakeogh <github.com@v6y.net>
2019-10-28 18:29:20 -0700

    auto-commit

--- raidcalc/raidcalc.py
+++ raidcalc/raidcalc.py
@@ -123,10 +123,10 @@ def raid(toraid, group_size, level):
     dev_count = len(toraid)
     if group_size == "all":
         group_size = dev_count
-    if group_size == "split":
+    elif group_size == "split":
         if not dev_count % 2 == 0:
             raise ValueError("Error: split requires an even number of devices")
-        group_size = dev_count / 2
+        group_size = int(dev_count / 2)
     else:
         group_size = int(group_size)
     grouped = group(toraid, group_size)
@@ -134,22 +134,30 @@ def raid(toraid, group_size, level):
     if VERBOSE:
         ic(grouped)
     if level == "mirror":
-        if dev_count < 2 or group_size < 2:
+        if dev_count < 2:
             raise ValueError("Error: mirror requires >= 2 devices")
+        if group_size < 2:
+            raise ValueError("Error: mirrored groups require >= 2 devices")
         raided = [group[0] for group in grouped]
     elif level == "stripe":
         raided = [sum(group) for group in grouped]
     elif level == "z1":
-        if dev_count < 3 or group_size < 3:
+        if dev_count < 3:
             raise ValueError("Error: z1 requires >= 3 devices")
+        if group_size < 3:
+            raise ValueError("Error: z1 groups require >= 3 devices")
         raided = [sum(group[:-1]) for group in grouped]
     elif level == "z2":
-        if dev_count < 4 or group_size < 4:
+        if dev_count < 4:
             raise ValueError("Error: z2 requires >= 4 devices")
+        if group_size < 4:
+            raise ValueError("Error: z2 groups require >= 4 devices")
         raided = [sum(group[:-2]) for group in grouped]
     elif level == "z3":
-        if dev_count < 5 or group_size < 5:
+        if dev_count < 5:
             raise ValueError("Error: z3 requires >= 5 devices")
+        if group_size < 5:
+            raise ValueError("Error: z3 groups require >= 5 devices")
         raided = [sum(group[:-3]) for group in grouped]
     else:
         raise NotImplementedError("Error: unknown RAID level:", level)
